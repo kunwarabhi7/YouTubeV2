56d58628dc4f621cfab9b4979d00910e6156eafa
Abhishek <abhishekparmar162@gmail.com>
2022-11-17 23:47:32 +0530

    get result on feed

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,7 +1,6 @@
-import React,{useState} from 'react'
+import React,{useContext, useState} from 'react'
 import { BsSearch } from 'react-icons/bs'
-import Link from 'next/link';
-import Card from './Card';
+import { SearchContext } from '../Context';
 
 
 const options = {
@@ -14,11 +13,11 @@ const options = {
 
 
 const SearchBar = () => {
-const [search, setsearach] = useState('');
-const [box,setBox] = useState([]);
+const {box,setBox} = useContext(SearchContext)
+  const [search, setsearach] = useState('');
 
-    
-const getResult = () => {
+  const getResult = (e) => {
+  e.preventDefault();
     fetch(`https://youtube-v31.p.rapidapi.com/search?q=${search}&part=snippet%2Cid&regionCode=IN&maxResults=50&order=date`, options)
       .then(response => response.json())
       .then(response => setBox(response.items))
@@ -26,15 +25,20 @@ const getResult = () => {
   }
 
   return (
-    <div className='flex'>
+    <>
+      <form onSubmit={getResult}>
+<div className='flex'>
+
          <input value={search} onChange={(e)=> setsearach(e.target.value)}  type="text" className='border border-gray-500 rounded-full w-[30rem] text-gray-400 rounded-r-none h-8 px-4 bg-black' placeholder='Search' />
 <BsSearch onClick={getResult} className='text-white p-1  border-2 -ml-[2px] border-gray-500 rounded-full rounded-l-none' size={30} />  
-{box.map((item)=>(
+</div>
+      </form>
+{/* {box.map((item)=>(
     <Link key={item.videoId} href={`https://www.youtube.com/watch?v=${item.id.videoId}`}>
     <Card  image={item.snippet.thumbnails.high.url}  title={item.snippet.title} channel={item.snippet.channelTitle} time={item.snippet.publishTime}  />
     </Link>
-))}
-  </div>
+))} */}
+  </>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import NavBar from '../components/NavBar'
 import '../styles/globals.css'
-
+import { SearchContext } from '../Context'
+import { useState } from 'react'
 function MyApp({ Component, pageProps }) {
+  const [box,setBox] = useState([]);
   return (
-    <>
+    <SearchContext.Provider value={{box,setBox}}>
 <NavBar />
     <Component {...pageProps} />
-    </>
+    </SearchContext.Provider>
     )
 }
 
