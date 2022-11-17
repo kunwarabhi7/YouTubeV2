dc13e111f158e3806de5fef414b1a009f23d6d9f
Abhishek <abhishekparmar162@gmail.com>
2022-11-17 17:11:47 +0530

    set  saerach bar

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -12,11 +12,12 @@ const options = {
 
 
 const SearchBar = () => {
-const [searach, setsearach] = useState()
+const [search, setsearach] = useState('');
 const [box,setBox] = useState([]);
+
     
 const getResult = () => {
-    fetch('https://youtube-v31.p.rapidapi.com/search?q=rajput&part=snippet%2Cid&regionCode=IN&maxResults=50&order=date', options)
+    fetch('https://youtube-v31.p.rapidapi.com/search?q={search}&part=snippet%2Cid&regionCode=IN&maxResults=50&order=date', options)
       .then(response => response.json())
       .then(response => console.log(response))
       .catch(err => console.error(err));
@@ -24,8 +25,10 @@ const getResult = () => {
 
   return (
     <div className='flex'>
-         <input type="text" className='border border-gray-500 rounded-full w-[30rem] text-gray-400 rounded-r-none h-8 px-4 bg-black' placeholder='Search' />
-<BsSearch onClick={getResult} className='text-white p-1  border-2 -ml-[2px] border-gray-500 rounded-full rounded-l-none' size={30} />    </div>
+         <input value={search} onChange={(e)=> setsearach(e.target.value)}  type="text" className='border border-gray-500 rounded-full w-[30rem] text-gray-400 rounded-r-none h-8 px-4 bg-black' placeholder='Search' />
+<BsSearch onClick={getResult} className='text-white p-1  border-2 -ml-[2px] border-gray-500 rounded-full rounded-l-none' size={30} />  
+
+  </div>
   )
 }
 
