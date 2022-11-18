af99a5785ed3df403b97f81e2949475f7601977c
Abhishek <abhishekparmar162@gmail.com>
2022-11-18 16:17:05 +0530

    deployment

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+
 import MainBar from "../components/MainBar";
 import SideBar from "../components/SideBar";
 import { SearchContext, SearchContext2 } from "../Context";
@@ -9,17 +9,18 @@ import Link from "next/link";
 export default function Home() {
   const { box, setbox } = useContext(SearchContext);
 
+
   return (
     <div className="flex">
       <SideBar />
     <div>
-
     {
       <div className="grid grid-cols-4">
         {box.map((item) => (
           <Link
-            key={item.videoId}
-            href={`https://www.youtube.com/watch?v=${item.id.videoId}`}
+          key={item.videoId}
+          // href={`https://www.youtube.com/watch?v=${item.id.videoId}`}
+          href={`/${item.id.videoId}`}
           >
             <Card
               image={item.snippet.thumbnails.high.url}
